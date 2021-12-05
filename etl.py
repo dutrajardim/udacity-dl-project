c01abb6616400b2a07b19e14574c2d792c7a649e
Rafael Dutra Jardim <rafaeldjardim@gmail.com>
2021-12-05 20:11:10 -0300

    created some tables schemas; created the comom pipeline; and some tests was ran in the spark notebook

--- etl.py
+++ etl.py
@@ -1,43 +1,59 @@
 import configparser
 from datetime import datetime
 import os
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
 
-config = configparser.ConfigParser()
-config.read('dl.cfg')
+def create_spark_conf(local=False):
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    spark_config = SparkConf()
+    spark_config.setAppName("Udacity - Data Lake Project")
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+    if local:
+        filepath = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "local.sparkconf.cfg"
+        )
+        config.read_file(open(filepath))
+        spark_config.setMaster(config["SPARK"]["master"])
+
+        for section in config.sections():
+            if section in ["SPARK"]:
+                continue
+
+            for item in config.items(section):
+                spark_config.set(*item)
+
+    return spark_config
 
 
-def create_spark_session():
-    spark = SparkSession \
-        .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
-        .getOrCreate()
+def create_spark_session(local=False):
+    conf = create_spark_conf(local=local)
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
     return spark
 
 
 def process_song_data(spark, input_data, output_data):
     pass
     # get filepath to song data file
-    # song_data = 
-    
+    # song_data =
+
     # read song data file
-    # df = 
+    # df =
 
     # extract columns to create songs table
-    # songs_table = 
-    
+    # songs_table =
+
     # write songs table to parquet files partitioned by year and artist
     # songs_table
 
     # extract columns to create artists table
-    # artists_table = 
-    
+    # artists_table =
+
     # write artists table to parquet files
     # artists_table
 
@@ -47,36 +63,36 @@ def process_log_data(spark, input_data, output_data):
     # log_data =
 
     # read log data file
-    # df = 
-    
-    # filter by actions for song plays
-    # df = 
+    # df =
 
-    # extract columns for users table    
-    # artists_table = 
-    
+    # filter by actions for song plays
+    # df =
+
+    # extract columns for users table
+    # artists_table =
+
     # write users table to parquet files
     # artists_table
 
     # create timestamp column from original timestamp column
     get_timestamp = udf()
-    # df = 
-    
+    # df =
+
     # create datetime column from original timestamp column
     get_datetime = udf()
-    # df = 
-    
+    # df =
+
     # extract columns to create time table
-    # time_table = 
-    
+    # time_table =
+
     # write time table to parquet files partitioned by year and month
     # time_table
 
     # read in song data to use for songplays table
-    # song_df = 
+    # song_df =
 
-    # extract columns from joined song and log datasets to create songplays table 
-    # songplays_table = 
+    # extract columns from joined song and log datasets to create songplays table
+    # songplays_table =
 
     # write songplays table to parquet files partitioned by year and month
     songplays_table
@@ -86,8 +102,8 @@ def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = ""
-    
-    process_song_data(spark, input_data, output_data)    
+
+    process_song_data(spark, input_data, output_data)
     process_log_data(spark, input_data, output_data)
 
 
